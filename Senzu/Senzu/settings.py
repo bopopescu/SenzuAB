9cb7f75c5aea19605ca9ef61748c96d57eba1945
alexisbatistabustavino <Alexis05batista@gmail.com>
2017-05-28 20:15:48 -0500

    static_root

--- Senzu/Senzu/settings.py
+++ Senzu/Senzu/settings.py
@@ -190,6 +190,6 @@ STATICFILES_DIRS = [
     '/var/www/html/Senzu/static/',
 ]
 
-STATIC_ROOT = '/var/www/html/Senzu/static/'
+STATIC_ROOT = '/var/www/html/Senzu/site/static/'
 
 CORS_ORIGIN_ALLOW_ALL = True
