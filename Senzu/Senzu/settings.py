5ca8ca63318bdc146d24e8cf2ba93480eef95d5a
alexisbatistabustavino <Alexis05batista@gmail.com>
2017-05-28 20:05:06 -0500

    lo mismo

--- Senzu/Senzu/settings.py
+++ Senzu/Senzu/settings.py
@@ -186,9 +186,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 #STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    '/var/www/html/Senzu/static/',
-]
+#    os.path.join(BASE_DIR, "static"),
+#    '/var/www/html/Senzu/static/',
+#]
 
 STATIC_ROOT = '/var/www/html/Senzu/static/'
 
