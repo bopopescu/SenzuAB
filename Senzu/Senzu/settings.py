ef6556aac5be3b72371a4d7a7df17ac45dbd2aa4
alexisbatistabustavino <Alexis05batista@gmail.com>
2017-10-15 16:51:00 -0500

    se sube el environment para poder ejecutar en el servidor

--- Senzu/Senzu/settings.py
+++ Senzu/Senzu/settings.py
@@ -134,7 +134,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'senzupy',
         'USER': 'root',
-        'PASSWORD': 'root',
+        'PASSWORD': '123456',
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {
